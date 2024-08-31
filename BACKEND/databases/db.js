b95773b977707f08a128550233a2881ef9736603
bhagattt <aryanbhagat850@gmail.com>
2024-09-01 00:19:25 +0530

    Login/Signup Auth

--- BACKEND/databases/db.js
+++ BACKEND/databases/db.js
@@ -1,18 +1,17 @@
-const mongoose=require('mongoose');
-
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://aryangg:Aryan%402024@cluster0.dhqj2.mongodb.net/library?retryWrites=true&w=majority', {
-        useNewUrlParser: true,  
-        useUnifiedTopology: true 
-      });
-      console.log('MongoDB connected successfully');
-    } catch (error) {
-      console.error('MongoDB connection error:', error.message);
-      process.exit(1);
-    }
-  };
-  
-  module.exports = connectDB;
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://aryangg:Aryan@2024@sihcluster.kumcg.mongodb.net/auth?retryWrites=true&w=majority&appName=SIHCLUSTER', {
+      useNewUrlParser: true,  
+      useUnifiedTopology: true 
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
